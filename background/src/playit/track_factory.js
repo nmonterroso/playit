100a637836b2b7a1a7ee599a4e6eb38f4d382040
nmonterroso <nelsonmonterroso@gmail.com>
2014-05-13 08:12:53 -0700

    remove backbone dependency on track factory

--- background/src/playit/track_factory.js
+++ background/src/playit/track_factory.js
@@ -1,28 +1,23 @@
 define(
 	[
 		'underscore',
-		'backbone',
 		'tracks/mixing.dj',
 		'tracks/zippyshare.com',
 	],
-	function(_, Backbone, mixing_dj, zippyshare) {
+	function(_, mixing_dj, zippyshare) {
 		'use strict';
 
-		var track = Backbone.Model.extend({}, {
-			factory: function(url) {
-				var detected_type = _.find([mixing_dj, zippyshare], function(track_type) {
-					return track_type.can_play(url);
-				});
+		return function(url) {
+			var detected_type = _.find([mixing_dj, zippyshare], function(track_type) {
+				return track_type.can_play(url);
+			});
 
-				if (!detected_type) {
-					console.error("unable to determine track type", url);
-					return null;
-				}
-
-				return new detected_type({source_url: url});
+			if (!detected_type) {
+				console.error("unable to determine track type", url);
+				return null;
 			}
-		});
 
-		return track.factory;
+			return new detected_type({source_url: url});
+		};
 	}
 );
